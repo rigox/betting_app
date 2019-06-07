cdd5851f4ee03300d304944360a8cd67257ed964
rigox <rigoc1994@gmail.com>
2019-06-07 19:42:19 -0400

    Admin can now close Contracts and money will be redistributed

--- src/components/Admin/Modal.js
+++ src/components/Admin/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cookies from '../../api/cookies';
 import { Button, Header, Image, Modal ,Form, Checkbox} from 'semantic-ui-react'
 import axios from 'axios';
 axios.defaults.baseURL ="http://localhost:4000";
@@ -26,13 +27,20 @@ onChangeHandler=(e)=>{
 
  componentWillReceiveProps(nextProps,prevProps){
        this.setState({open:nextProps.open})
-  
  }
 
  closeContract=()=>{
-   
-    this.setState({open:false})
-    console.log(this.state)
+    var config = {headers:{"Authorization":"Bearer " + Cookies.get("password")}}
+    axios.put('Contract/close_contract',{
+        result:this.state.result,
+        _id:this.state._id
+    },config)
+    .then(res=>console.log(res.data))
+    .catch(err=>console.log(err))
+    this.setState({open:false})}
+
+cancel=()=>{
+     this.setState({open:false})
 }
 
 
@@ -52,7 +60,9 @@ onChangeHandler=(e)=>{
       <label>Enter Winner</label>
       <input placeholder='Enter Winner of Contest' type="text" name="result"  onChange={this.onChangeHandler} />
     </Form.Field>
-    <Button onClick={this.closeContract} type='submit'>Submit</Button>
+    <Button className="ui blue button" onClick={this.closeContract} type='submit'>Close Contract</Button>
+    <Button  className="ui red button" onClick={this.cancel} type='submit'>Cancel</Button>
+
   </Form>
 
 
